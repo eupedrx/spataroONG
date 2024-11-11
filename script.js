a5650f7cfd99571be6ff2053f8bc976218d5a478
PedroSaturno <pedro1saturno@gmail.com>
2024-11-10 23:29:48 -0300

    adição novos arquivos

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).on("scroll", function() {
-    const scrollPosition = $(this).scrollTop(); // Pega a posição de rolagem
-    $(".parallax").each(function() {
-        const speed = $(this).data("speed"); // Obtém o valor de 'data-speed'
-        const yPos = -(scrollPosition * speed); // Calcula a posição Y
-        $(this).css("transform", `translateY(${yPos}px)`); // Aplica a transformação
-    });
-});
-
